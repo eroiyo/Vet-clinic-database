6bd37cee95f12bd1dd552c4457d2e1d880c59291
eroiyo <59938389+eroiyo@users.noreply.github.com>
2021-09-27 15:52:00 -0400

    created all querys

--- queries.sql
+++ queries.sql
@@ -13,4 +13,20 @@ SELECT * from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
 /*List name and escape attempts of animals that weigh more than 10.5kg*/
 
-SELECT (name, escape_attempts) from animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
+SELECT (name, escape_attempts) from animals WHERE weight_kg > 10.5;
+
+/*List date of birth of all animals named either "Agumon" or "Pikachu".*/
+
+SELECT (date_of_birth) from animals WHERE name = "Agumon" OR "Pikachu";
+
+/*List the name of all animals that are neutered and have less than 3 escape attempts.*/
+
+SELECT (name) from animals WHERE neutered = true AND escape_attempts < 3;
+
+/*List the name of all animals born between 2016 and 2019.*/
+
+SELECT (name) from animals WHERE date_of_birth BETWEEN "2016-1-1" AND "2019-12-31";
+
+/*Find all animals whose name ends in "mon".*/
+
+SELECT * from animals WHERE name LIKE '%mon%
